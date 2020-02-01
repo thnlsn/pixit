cc94695a8b6a7b64494f037830c1fe548467e784
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-02-01 00:46:02 -0800

    database connected

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
-    zipcode: {
+    zip: {
         type: Number,
         required: true
     },
--- server.js
+++ server.js
@@ -2,4 +2,18 @@
 const express = require('express');
 connectDB = require('./config/db');
 const app = express();
-const path = require('path');
+/* const path = require('path'); */
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+
+////// Define routes
+/* app.use('/api/user', require('./routes/user')); // use the routes */
+
+// If there is an environment variable for hosted port, use it, otherwise use 1000 (development)
+const PORT = process.env.PORT || 1000;
+
+// Start server with PORT
+app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
