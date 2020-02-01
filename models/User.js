f09067a614697cb3f783333aab660e5af3427290
thnlsn <52093425+thnlsn@users.noreply.github.com>
2020-01-31 23:12:42 -0800

    Schema for users created

--- models/User.js
+++ models/User.js
@@ -1,1 +1,54 @@
 const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    zipcode: {
+        type: Number,
+        required: true
+    },
+    lbs: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    contributions: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    starts: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    daysActive: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    daysCleaned: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    pixPoints: {
+        type: Number,
+        required: true,
+        default: 0
+    }
+});
+
+module.exports = mongoose.model('user', UserSchema); // ('modelname', Schema)
